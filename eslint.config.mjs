0ce1ee9d0cdd652148fa9ad0cb84d71c1fe6cc18
Paul Anderson <pauldevop@gmail.com>
2025-05-04 21:22:57 -0300

    fix/adjust-worflows

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,11 @@ const eslintConfig = [
     'next/typescript',
     '@rocketseat/eslint-config/next',
   ),
+  {
+    rules: {
+      '@typescript-eslint/no-namespace': 'off',
+    },
+  },
 ]
 
 export default eslintConfig
